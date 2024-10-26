9e240221b25bbb3808ff604ec6f73a5cc649e778
devo243 <cbrian@mit.edu>
2024-10-26 18:39:29 -0400

    Modified featuring system

--- server/concepts/featuring.ts
+++ server/concepts/featuring.ts
@@ -13,14 +13,16 @@ export interface FeaturingDoc extends BaseDoc {
 export default class FeaturingConcept {
   public readonly features: DocCollection<FeaturingDoc>;
   public readonly minimumAttention: number;
+  public readonly timeLimit: number;
 
-  constructor(collectionName: string, minimumAttention: number) {
+  constructor(collectionName: string, minimumAttention: number, dayLimit: number) {
     this.features = new DocCollection<FeaturingDoc>(collectionName);
     this.minimumAttention = minimumAttention;
+    this.timeLimit = dayLimit * 24 * 60 * 60;
   }
 
   async promote(item: ObjectId, attention: number) {
-    this.assertItemIsNotFeatured(item);
+    await this.assertItemIsNotFeatured(item);
 
     if (attention >= this.minimumAttention) {
       await this.features.createOne({ item: item });
@@ -31,7 +33,7 @@ export default class FeaturingConcept {
   }
 
   async depromote(item: ObjectId, attention: number) {
-    this.assertItemIsFeatured(item);
+    await this.assertItemIsFeatured(item);
 
     if (attention < this.minimumAttention) {
       await this.features.deleteOne({ item: item });
@@ -41,22 +43,48 @@ export default class FeaturingConcept {
     }
   }
 
+  async checkPromoted() {
+    const features = await this.features.readMany({});
+
+    const currentTime = new Date();
+
+    await Promise.all(
+      features.map(async (feature) => {
+        const timeDiff = (currentTime.getTime() - feature.dateCreated.getTime()) / 1000;
+
+        if (timeDiff > this.timeLimit) {
+          await this.features.deleteOne({ _id: feature._id });
+        }
+      }),
+    );
+  }
+
   async getFeatured() {
+    await this.checkPromoted();
+
     return await this.features.readMany({}, { sort: { _id: -1 } });
   }
 
   async assertItemIsFeatured(item: ObjectId) {
-    const feature = this.features.readOne({ item: item });
+    const feature = await this.features.readOne({ item: item });
 
-    if (!item) {
+    if (!feature) {
       throw new NotFoundError("Item is not featured!");
+    } else {
+      const currentTime = new Date();
+
+      const timeDiff = (currentTime.getTime() - feature.dateCreated.getTime()) / 1000;
+
+      if (timeDiff > this.timeLimit) {
+        throw new NotAllowedError("Item is too old to be featured!");
+      }
     }
   }
 
   async assertItemIsNotFeatured(item: ObjectId) {
-    const feature = this.features.readOne({ item: item });
+    const feature = await this.features.readOne({ item: item });
 
-    if (item) {
+    if (feature) {
       throw new NotAllowedError("Item is already featured!");
     }
   }
